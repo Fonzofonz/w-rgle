bb73e325f4f5ac0f9da5843736919fe3cdda4e72
Fonzofonz <awgaug@gmail.com>
2024-08-24 15:10:53 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+
+PORT = 8080
+
+class HelloHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        # Setze den Antwort-Header
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        # Schreibe den Inhalt der Webseite
+        self.wfile.write(b"Hello, World!")
+
+with socketserver.TCPServer(("", PORT), HelloHandler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()

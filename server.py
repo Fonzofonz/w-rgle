e254b1c3c496be0d1d5fc5ff72bf365df59f32da
Fonzofonz <awgaug@gmail.com>
2024-08-24 17:20:58 +0200

    Update server.py

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ class HelloHandler(http.server.SimpleHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         # Schreibe den Inhalt der Webseite
-        self.wfile.write(b"Hello, World!")
+        self.wfile.write(b"Hello, World oder so!")
 
 with socketserver.TCPServer(("", PORT), HelloHandler) as httpd:
     print(f"Serving on port {PORT}")
